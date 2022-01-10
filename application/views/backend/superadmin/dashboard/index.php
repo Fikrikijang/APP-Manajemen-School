9ae0338d1417f0b254a0dda6ebb8d2158dfb4487
adeATS01 <adhefikri98@gmail.com>
2022-01-10 14:20:23 +0700

    commit navbar superadmin

--- application/views/backend/superadmin/dashboard/index.php
+++ application/views/backend/superadmin/dashboard/index.php
@@ -4,7 +4,6 @@
     <div class="card">
       <div class="card-body py-2">
         <h4 class="page-title"> <i class="mdi mdi-view-dashboard title_icon"></i> <?php echo get_phrase('dashboard'); ?> </h4>
-        <h5>SELAMAT DATANG DI WEBSITE KAMI.</h5>
       </div> <!-- end card body-->
     </div> <!-- end card -->
   </div><!-- end col-->
